cf524a289b5106924ca05878d7db15f173ea3213
hayashinoshun <s.y1014torimaru@gmail.com>
2022-08-23 22:44:57 +0900

    rails devise認証機能作成 WIP 最終まで完了したが反映されていない？ 引き続き修正案件

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  # get 'statics/index'
+  devise_for :users, controllers: {
+    # sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get '/sign_in', to: 'users/sessions#new'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
