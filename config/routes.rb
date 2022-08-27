b9490bfeff5368f407f9eba1104289194c35481d
hayashinoshun <s.y1014torimaru@gmail.com>
2022-08-27 21:48:33 +0900

    showアクションにて、投稿一つ一つを参照できるようにする。WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "posts/index" , to: "posts#index"
-  get "posts/index"
+  get "posts/:id", to: "posts#show"
+  
   get "/", to: "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "home#about"
