e8af4c00817ee5edd32e4d0a0f7a731d0e8f434e
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-02 00:59:40 +0900

    投稿記事に対して編集項目を追加 WIP top,aboutの背景と参考画像の編集

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
   post "posts/create", to: "posts#create"
+  get "posts/:id/edit", to: "posts#edit"
   get "/", to: "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "home#about"
