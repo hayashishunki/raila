185b296622294370aba1628de6e7dad8ad84b2bc
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-02 21:45:27 +0900

    updateアクション作成完了 投稿の更新ができるようになりました。

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "posts/:id", to: "posts#show"
   post "posts/create", to: "posts#create"
   get "posts/:id/edit", to: "posts#edit"
+  post "posts/:id/update", to: "posts#update"
+
   get "/", to: "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "about", to: "home#about"
