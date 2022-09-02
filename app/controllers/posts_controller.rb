3b73dc622345682b328ee05507ffdba3b2b95ea6
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-02 23:10:33 +0900

    添削処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,6 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @post.destroy
     
-    redirect_to("/posts/index")
+    redirect_to "/posts/index"
   end
 end
