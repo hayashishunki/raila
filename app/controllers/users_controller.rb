230cecf76f92349ad2cab58a8639d9805e6d264c
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-06 23:36:26 +0900

    ユーザー作成機能　追加 name,email未記入の場合エラー emailはインデックスかぶり禁止のエラーを出す  完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,17 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
-    @users = User.new(name: params[:name], email: params[:email])
-    @users.save
-    redirect_to "/users/#{@users.id}"
+    @user = User.new(name: params[:name], email: params[:email])
+    if @user.save
+      flash[:notice] = "ユーザー登録が完了しました"
+      redirect_to "/users/#{@user.id}"
+    else
+      render "users/new"
+    end
   end
 
 end
