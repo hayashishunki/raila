546666191dbcbc55244d573f967bca9a347f4db2
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-07 23:10:14 +0900

    ユーザー編集機能の作成完了 エラー文も表示確認OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @users = User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
   end
 
   def new
@@ -18,6 +18,22 @@ class UsersController < ApplicationController
       redirect_to "/users/#{@user.id}"
     else
       render "users/new"
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if @user.save
+      flash[:notice] = "投稿の編集が完了しました"
+      redirect_to "/users/#{@user.id}"
+    else
+      render "users/edit"
     end
   end
 
