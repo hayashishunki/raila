78ee9e4a16d1e7b7130eec4b18c4079176b4cb67
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-13 23:40:46 +0900

    とくゆーさん修正箇所のいくつか修正しました。 WIP ストロングパラメータで受け取れるように migrateファイルにバリデーション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,25 @@
 class PostsController < ApplicationController
+  
+  # require 'active_support/all'
+
   def index
     @posts = Post.all.order(created_at: :desc)
+    if @post.blank?
+      return @post
+    else
+      flash[:notice] = "値が存在しません管理者に問い合わせてください"
+      redirect_to "/"
+    end
   end
 
   def show
-    # [余裕あれば修正]
-    # idでPost.find(params[:id])にしない理由はありますか？
-    # そもそも詳細画面で@postがnilになるケースはエラー扱いなので、フロントに渡す前にここでエラー処理した方がいいです。
-    # 他の部分も同様です。
     @post = Post.find(params[:id])
+    if @post.present?
+        return @post
+    else
+      flash[:notice] = "データが存在しませんやり直してください"
+      redirect_to "/posts/index"
+    end
   end
 
   def new
@@ -16,49 +27,46 @@ class PostsController < ApplicationController
   end
 
   def create
-  # [やっておいた方が有利]
-  # データ保存の際はストロングパラメータを使った方がいいです。
-  # 参考サイト https://qiita.com/ozackiee/items/f100fd51f4839b3fdca8
-    @post = Post.new(content: params[:content])
-    # 一つ余計なスペースがあるので削除
-
-    # ""と''の違い
-    # '' => 文字列のみで構成された際に使用 (例) 'テストです'
-    # "" => 変数と組み合わせる際に使用 (例) "test = 'テスト'  #{test}です"
-
-    # [やっておいた方が有利]
-    # @post.saveより@post.save!を使ってエラーを明確した方が良いです。
-    # save!に関する参考サイト https://techacademy.jp/magazine/22082
-     if @post.save
-      # [余裕あれば修正]
-      # 以下の書き方だと1行で書けたりします
-      # redirect_to posts_path, flash: { notice: '投稿を作成しました' }
-      flash[:notice] = "投稿を作成しました"
-      redirect_to "/posts/index"
-     else
+    @post = Post.create(post_params)
+    begin
+      @post.save!
+      redirect_to posts_index_path, flash: { notice: '投稿を作成しました' }
+    rescue => exception
       render "posts/new"
-     end
+    end
   end
 
   def edit
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
+    if @post.present?
+      return @post
+    else
+      flash[:notice] = "データが存在しませんやり直してください"
+      redirect_to "/posts/#{@post.id}/edit"
+    end
   end
 
   def update
-    @post = Post.find_by(id: params[:id])
-    @post.content = params[:content]
-    if @post.save
+    @post = Post.find(params[:id])
+    # @post.content = Post.update(post_params)
+    begin
+      @post.save!
       flash[:notice] = "投稿を編集しました"
       redirect_to "/posts/index"
-    else
+    rescue => exception
       render "posts/edit"
     end
   end
 
   def destroy
-    @post = Post.find_by(id: params[:id])
+    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
     redirect_to "/posts/index"
+  end
+
+  private
+  def post_params
+    params.permit(:content)
   end
 end
--- db/migrate/20220825134356_create_posts.rb
+++ db/migrate/20220825134356_create_posts.rb
@@ -1,10 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      # 説明文も加えておくと良さげです。
-      # validation系はDB本体にもかけておくとRails側でチェック漏れた時にもデータ登録されないので書いておきましょう
-      # (例) t.integer :hoge, null: false, comment: 'ほげ'
-      t.text :content
+      t.text :content, null: false, comment: '内容'
 
       t.timestamps
     end
