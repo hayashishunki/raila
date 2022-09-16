51195230e6dc4ef38d603f4d31e0d92b55e747e1
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-16 17:32:28 +0900

    ログアウト機能実装 sessionにnil代入して空にする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(name: params[:name], email: params[:email], image_name: 'icon1.png')
+    @user = User.new(name: params[:name],
+                    email: params[:email],
+                    image_name: 'icon1.png',
+                    password: params[:password]
+                  )
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = 'ユーザー登録が完了しました'
       redirect_to "/users/#{@user.id}"
     else
