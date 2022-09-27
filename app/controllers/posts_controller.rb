05d878074b8f093ec3a0914c300233b5c8208b6a
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-27 23:51:35 +0900

    show詳細画面にて投稿者の画像と名前を表示させる。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
+    @user = User.find_by(id: @post.user_id)
+  rescue
     redirect_to posts_index_path, flash: { notice: 'データが存在してませんやり直してください' } if @post.blank?
   end
 
