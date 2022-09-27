5fbf8b6dfa84eedd6492ca2faed58124762012ff
hayashinoshun <s.y1014torimaru@gmail.com>
2022-09-27 16:34:41 +0900

    投稿内容と投稿者に同一性をもたせる為、user_idカラムを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,16 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  email      :string
+#  email      :string           not null
 #  image_name :string
-#  name       :string
+#  name       :string           not null
 #  password   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
 #
 require "test_helper"
 
